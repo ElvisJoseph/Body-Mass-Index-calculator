edbeb5574a4e7f9cf9c3468e41a0e39b87ee24ff
Elvis Mapesa <71148339+ElvisJoseph@users.noreply.github.com>
2022-10-14 03:31:09 +0300

    Update BMI calculator.py

--- BMI calculator.py
+++ BMI calculator.py
@@ -5,10 +5,13 @@ height_sq = height*height
 bmi = weight/height_sq
 
 if bmi < 18.5:
-    print("underweight")
+    print("Underweight")
     
 elif bmi >= 18.5 and  bmi < 25:
-    print("normal")
+    print("Normal")
     
-elif bmi >= 30:
-    print("obesity")
+elif bmi >= 25 and bmi < 30:
+    print("Overweight")
+    
+else:
+    print("Obesity")
